4762742fa1f60fc76517f49b56dfbc0991687b6c
figs24 <pwsvnp@gmail.com>
2022-06-15 20:58:57 +0800

    roadmap: slicing halaman landing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,15 @@ import { Container, Button, Box, Image, Heading, Text } from '@chakra-ui/react';
 import Navbar from './component/Navbar';
 import Login from './component/Login'
 import Register from './component/Register'
+import LandingPage from './component/Landing-page';
 
 function App() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       {/* <Login /> */}
-      <Register />
+      {/* <Register /> */}
+      <LandingPage />
     </>
   );
 }
