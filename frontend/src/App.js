bdddf556d98d6c31501b8af7bc9ceeaa26abf88b
dikamuharman <dika1254@gmail.com>
2022-06-17 08:36:22 +0700

    style: :art: fix indentation and format code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,17 @@
 import { Container, Button, Box, Image, Heading, Text } from '@chakra-ui/react';
-import Login from './component/Login'
-import Register from './component/Register'
+import Login from './component/Login';
+import Register from './component/Register';
 import LandingPage from './component/Landing-page';
 import TeacherDashboard from './component/Teacher-dashboard';
-import FormAddProfesi from './component/Form-addProfesi'
+import FormAddProfesi from './component/Form-addProfesi';
 
 function App() {
   return (
     <>
-      <Login />
-      <Register />
+      {/* <Login />
+      <Register /> */}
       <LandingPage />
-      <TeacherDashboard/>
+      {/* <TeacherDashboard/> */}
       {/* <FormAddProfesi/> */}
     </>
   );
--- frontend/src/component/Form-addProfesi.jsx
+++ frontend/src/component/Form-addProfesi.jsx
@@ -1,24 +1,24 @@
 import {
-    FormControl,
-    FormLabel,
-    FormErrorMessage,
-    FormHelperText,
-    Input,
-  } from '@chakra-ui/react'
-import Footer from './Footer'
-import Header from './Header'
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+} from '@chakra-ui/react';
+import Footer from './Footer';
+import Header from './Header';
 
 // bentar vina cape ngodinggg wkwkwk, nanti dilanjuttttt
 
-  export default function formAdd (){
-      return(
-          <>
-        <Header/>
-        <FormControl>
-        <FormLabel htmlFor='email'>Email address</FormLabel>
-        <Input id='email' type='email' />
+export default function formAdd() {
+  return (
+    <>
+      <Header />
+      <FormControl>
+        <FormLabel htmlFor="email">Email address</FormLabel>
+        <Input id="email" type="email" />
       </FormControl>
-      <Footer/>
-      </>
-      )
-  }
+      <Footer />
+    </>
+  );
+}
