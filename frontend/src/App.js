07c408942c5a5dd212a7702d5f5ae0b507a59d25
figs24 <pwsvnp@gmail.com>
2022-06-21 22:42:00 +0800

    add: router detail roadmap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,6 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Maintener from './pages/Maintener-dashboard';
 import Teacher from './pages/Teacher-dashboard';
-
 import AddMateri from './pages/Form-addMateri';
 import ListRoadmap from './pages/List-roadmap';
 import DetailRoadmap from './pages/Detail-roadmap';
