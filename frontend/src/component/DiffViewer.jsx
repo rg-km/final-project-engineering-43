970c0c39ee10aafb0a502398f5398d7c7ecb9d09
figs24 <pwsvnp@gmail.com>
2022-06-22 23:03:15 +0800

    style: fix indentation and format code in diffviewer

--- frontend/src/component/DiffViewer.jsx
+++ frontend/src/component/DiffViewer.jsx
@@ -1,21 +1,21 @@
 import ReactDiffViewer from "react-diff-viewer";
 
-export default function DiffViewer(){
-    return(
-        <ReactDiffViewer
-        leftTitle="Materi Lama"
-        oldValue={materiLama}
-        rightTitle="Materi Baru"
-        newValue={materiBaru}
-        />
-    )
+export default function DiffViewer() {
+  return (
+    <ReactDiffViewer
+      leftTitle="Materi Lama"
+      oldValue={materiLama}
+      rightTitle="Materi Baru"
+      newValue={materiBaru}
+    />
+  );
 }
 
-const materiLama=`
+const materiLama = `
 Persamaan 2 Variable
 sebuah bentuk relasi sama dengan pada bentuk aljabar yang memiliki dua variabel dan keduanya berpangkat satu.
 `;
-const materiBaru=`
+const materiBaru = `
 Persamaan 2 Variable
 sebuah bentuk relasi sama dengan pada bentuk aljabar yang memiliki dua variabel dan keduanya berpangkat satu.
 Dikatakan Persamaan Linear karena pada bentuk persamaan ini jika digambarkan dalam bentuk grafik, maka akan terbentuk sebuah grafik garis lurus (linear).
