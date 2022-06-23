199ba012cdc28b67a0fdf2395c7eb3114af53ba9
dikamuharman <dika1254@gmail.com>
2022-06-23 10:01:45 +0700

    chore: :technologist: add conditional to handle file input

--- frontend/src/hooks/useFormHook.js
+++ frontend/src/hooks/useFormHook.js
@@ -3,7 +3,11 @@ import { useState } from 'react';
 const useFormHook = () => {
   const [formData, setFormData] = useState([]);
   const onChange = (name) => (e) => {
-    setFormData({ ...formData, [name]: e.target.value });
+    if (e.target.type === 'file') {
+      setFormData({ ...formData, [name]: e.target.files[0] });
+    } else {
+      setFormData({ ...formData, [name]: e.target.value });
+    }
   };
   const handleSubmit = (onSubmit) => (e) => {
     e.preventDefault();
