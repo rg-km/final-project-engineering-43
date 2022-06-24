39df52999b7b081a08560e0250626808edfa82ff
dikamuharman <dika1254@gmail.com>
2022-06-24 17:05:32 +0700

    feat: :sparkles: add Richtext editor component and selected component

--- frontend/src/pages/teacher/Form-addMateri.jsx
+++ frontend/src/pages/teacher/Form-addMateri.jsx
@@ -1,14 +1,24 @@
 import {
   Flex,
   Box,
+  Container,
   Stack,
   Link,
   Button,
   useColorModeValue,
+  FormControl,
+  FormLabel,
+  Textarea,
+  Heading,
+  Select,
 } from '@chakra-ui/react';
 import Footer from '../../component/Footer';
-import Forminput from '../../component/FormInputFile';
+import Forminput from '../../component/Forminput';
+import FormInputFile from '../../component/FormInputFile';
 import useFormHook from '../../hooks/useFormHook';
+import { useQuill } from 'react-quilljs';
+import 'quill/dist/quill.snow.css';
+import { useEffect } from 'react';
 
 /**
  *
@@ -17,16 +27,24 @@ import useFormHook from '../../hooks/useFormHook';
  */
 
 export default function FormAddMateri() {
+  const { quill, quillRef } = useQuill();
   const { onChange, handleSubmit } = useFormHook();
   const onSubmit = (value) => {
     console.log(value);
   };
 
+  useEffect(() => {
+    if (quill) {
+      quill.on('text-change', () => {
+        console.log(quill.root.innerHTML);
+      });
+    }
+  }, [quill]);
+
   return (
     <>
       <Flex
         minH={'100vh'}
-        align={'center'}
         justify={'center'}
         bg={useColorModeValue('white', 'gray.700')}
       >
@@ -35,49 +53,39 @@ export default function FormAddMateri() {
             rounded={'lg'}
             bg={useColorModeValue('gray.50', 'gray.800')}
             boxShadow={'lg'}
-            p={16}
+            p={8}
           >
+            <Heading mb="8">Tambah materi</Heading>
             <Stack spacing={4} as="form" onSubmit={handleSubmit(onSubmit)}>
-              <Forminput
-                id="tittle"
-                label="Tittle"
-                type="text"
-                onChange={onChange('tittle')}
-              />
-              <Forminput
+              <FormInputFile
                 id="thumbnail"
                 label="Thumbnail"
                 //harusnya image ya?
                 type="file"
                 onChange={onChange('thumbnail')}
               />
-              {/* <Textarea/> */}
               <Forminput
-                id="description"
-                label="Description"
-                type="textarea"
-                onChange={onChange('description')}
-              />
-              <Flex>
-                <Forminput
-                  id="tingkatan"
-                  label="Tingkatan"
-                  type="text"
-                  onChange={onChange('tingkatan')}
-                />
-                <Forminput
-                  id="kategori"
-                  label="Kategori"
-                  type="text"
-                  onChange={onChange('kategori')}
-                />
-              </Flex>
-              <Forminput
-                id="konten"
-                label="Konten"
+                id="judul"
+                label="Judul"
                 type="text"
-                onChange={onChange('konten')}
+                onChange={onChange('judul')}
               />
+              {/* <Textarea/> */}
+              <FormControl>
+                <FormLabel>Pilih profesi</FormLabel>
+                <Select placeholder="Profesi">
+                  <option value="1">Dokter</option>
+                  <option value="2">Insinyur</option>
+                </Select>
+              </FormControl>
+              <FormControl>
+                <FormLabel htmlFor="deskripsi">Deskripsi</FormLabel>
+                <Textarea onChange={onChange('deskripsi')} />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Content</FormLabel>
+                <div ref={quillRef} />
+              </FormControl>
               <Stack spacing={10}>
                 <Flex justify={'space-between'}>
                   <Link mr={10} color={'#36435A'} type="submit">
