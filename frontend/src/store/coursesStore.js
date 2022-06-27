bacff6e908fb55398a775bf84028d31908d83cc5
dikamuharman <dika1254@gmail.com>
2022-06-27 13:53:29 +0700

    perf: :construction: add presist state to prevent flashing data between page

--- frontend/src/store/coursesStore.js
+++ frontend/src/store/coursesStore.js
@@ -1,18 +1,22 @@
 import create from 'zustand';
+import { persist } from 'zustand/middleware';
 
-const useCoursesStore = create((set, get) => ({
-  length: 0,
-  courses: [],
-  setCourses: (courses) =>
-    set((state) => ({ courses, length: state.courses.length })),
-  filtered: (roadmap) => {
-    const courses = get().courses;
-    return {
-      roadmap,
-      courses: courses.filter((course) => course.RoadmapId === roadmap.id),
-    };
-  },
-  clearcourses: () => set({ length: 0, courses: [] }, true),
-}));
+const useCoursesStore = create(
+  persist((set, get) => ({
+    length: 0,
+    courses: [],
+    setCourses: (courses) =>
+      set((state) => ({ courses, length: state.courses.length })),
+    filtered: (roadmap) => {
+      const courses = get().courses;
+      return {
+        roadmap,
+        courses: courses.filter((course) => course.RoadmapId === roadmap.id),
+      };
+    },
+    getCourses: (id) => get().courses.find((course) => course.id === id),
+    clearcourses: () => set({ length: 0, courses: [] }, true),
+  }))
+);
 
 export default useCoursesStore;
--- frontend/src/store/roadmapStore.js
+++ frontend/src/store/roadmapStore.js
@@ -1,15 +1,18 @@
 import create from 'zustand';
+import { persist } from 'zustand/middleware';
 
-const useRoadmapStore = create((set, get) => ({
-  length: 0,
-  roadmaps: [],
-  setRoadmaps: (roadmaps) =>
-    set((state) => ({ roadmaps, length: state.roadmaps.length })),
-  getRoadmap: (id) => {
-    const roadmaps = get().roadmaps;
-    return roadmaps.find((roadmap) => roadmap.id === id);
-  },
-  clearRoadmaps: () => set({ length: 0, roadmaps: [] }),
-}));
+const useRoadmapStore = create(
+  persist((set, get) => ({
+    length: 0,
+    roadmaps: [],
+    setRoadmaps: (roadmaps) =>
+      set((state) => ({ roadmaps, length: state.roadmaps.length })),
+    getRoadmap: (id) => {
+      const roadmaps = get().roadmaps;
+      return roadmaps.find((roadmap) => roadmap.id === id);
+    },
+    clearRoadmaps: () => set({ length: 0, roadmaps: [] }),
+  }))
+);
 
 export default useRoadmapStore;
